eba2b7164957c5bc99605cc880bebb229001f99f
Fabian Unger <unger.fabian@posteo.de>
2019-12-11 18:34:55 +0100

    pass data in the props

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet,Button,Text } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
-export default function CameraScreen() {
+
+export default function CameraScreen(props) {
     return (
         <ScrollView style={styles.container}>
             {/**
              * Go ahead and delete ExpoLinksView and replace it with your content;
              * we just wanted to provide you with some helpful links.
              */}
-            <ExpoLinksView />
+
+            <Button
+                onPress={() => props.navigation.goBack()}
+                title="Dismiss"
+            />
+            <Text style={styles.getStartedText}>  {JSON.stringify(props.navigation.getParam('otherParam', 'default value'))}</Text>
+
         </ScrollView>
     );
 }
 
 CameraScreen.navigationOptions = {
-    title: 'Links',
+    title: 'Camera',
 };
 
 const styles = StyleSheet.create({
@@ -23,5 +30,11 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 15,
         backgroundColor: '#fff',
+    },
+    getStartedText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        lineHeight: 24,
+        textAlign: 'center',
     },
 });
